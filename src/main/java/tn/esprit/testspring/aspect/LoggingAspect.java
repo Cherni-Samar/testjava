0f5cf3b24f1898f35b8915acacf575407294facd
samar <chernisamar029@gmail.com>
2025-12-16 23:54:58 +0100

    work not fonctionnel1

--- src/main/java/tn/esprit/testspring/aspect/LoggingAspect.java
+++ src/main/java/tn/esprit/testspring/aspect/LoggingAspect.java
@@ -12,6 +12,7 @@ public class LoggingAspect {
     @AfterReturning("execution(* tn.esprit.testspring.service.*.*(..))")
     public void logMethod(JoinPoint jp) {
         System.out.println("Méthode exécutée ;: " + jp.getSignature().getName());
-shshshshshs
+        System.out.println("sarra zied rayen rawen ");
+
     }
 }
